b533997b4248c8c3d944e611f59d6806e500bd44
Arthurbsnrd <arthurbesnard1@outlook.fr>
2022-12-16 16:51:38 +0100

    début des routes nodeJS

---
+++ js/server.js
@@ -0,0 +1,40 @@
+const express = require('express');// la récupération d'express
+const mariadb = require('mariadb');
+var cors = require('cors')
+require('dotenv').config()
+
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD
+    });
+const app = express()
+app.use(express.json())
+app.use(cors()) // évite les erreurs xml
+
+app.get('/utilisateur', async(req, res)=>{
+    let conn = await pool.getConnection()
+    let resultat = await conn.query('SELECT * FROM utilisateur')
+    res.status(200).json(resultat)
+})
+
+app.post('/utilisateur', async(req, res)=>{
+    console.log("connexion bdd");
+    let conn = await pool.getConnection()
+    console.log("créer la requete")
+    await conn.query('INSERT INTO utilisateur(nom, mail, adresse, mdp) VALUES (?,?,?,?)',[req.body.nom,req.body.mail,req.body.adresse, req.body.mdp])
+    res.status(200).json({message: "Utilisateur créer"})
+})
+
+app.get('/produits', async(req, res)=>{
+    let conn = await pool.getConnection()
+    let resultat = await conn.query('SELECT * FROM produits')
+    res.status(200).json(resultat)
+})
+
+
+// server.listen(3000)
+app.listen(8000, () => { // ouverture du serveur sur le port 3000
+    console.log("Serveur à l'écoute") // afficher un message dans la console.
+})
