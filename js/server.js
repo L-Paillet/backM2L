5a86648b098d5802a51d3e618ca57bb776814d5b
laura <l.paillet@ecole-ipssi.net>
2023-03-18 12:33:45 +0100

    correction connexion server

--- js/server.js
+++ js/server.js
@@ -14,55 +14,54 @@ app.use(express.json())
 app.use(cors()) // évite les erreurs xml
 
 app.get('/utilisateur', async(req, res)=>{
-    let conn = await pool.getConnection()
+    const conn = await pool.getConnection();
     let resultat = await conn.query('SELECT * FROM utilisateur')
     res.status(200).json(resultat)
-    conn.release()
-
-
+    conn.release();
+    
 })
 
 //création d'utilisateur
 app.post('/utilisateur', async(req, res)=>{
     console.log("connexion bdd");
-    let conn = await pool.getConnection()
+    
+    const conn = await pool.getConnection()
     console.log("créer la requete")
     await conn.query('INSERT INTO utilisateur(nom, mail, adresse, mdp) VALUES (?,?,?,?)',[req.body.nom,req.body.mail,req.body.adresse, req.body.mdp])
     res.status(200).json({message: "Utilisateur créer"})
-    conn.release()
+    conn.release();
 
 })
 
 // connexion
 app.post('/login', async(req, res) =>{
 
-    let login = await pool.getConnection()
+    const login = await pool.getConnection()
 // Récupération des informations de connexion de l'utilisateur dans la requête
     const username = req.body.username;
     const password = req.body.password;
     const jwt = require('jsonwebtoken');
         // Vérification des informations de connexion de l'utilisateur dans la base de données SQL
-        let resultat = await login.query(`SELECT * FROM utilisateur WHERE nom = '${username}' AND mdp = '${password}'`);
+        const resultat = await login.query(`SELECT * FROM utilisateur WHERE nom = '${username}' AND mdp = '${password}'`);
         if (!resultat) {
         return res.status(401).send({ error: "Mauvais login" });
         }
+        console.log(resultat);
   
     // Si les informations sont correctes, génération d'un jeton de connexion
-    
     const token = jwt.sign({ userId: resultat[0].id }, process.env.JWT_SECRET);
-  
     // Envoi du jeton de connexion en réponse
-    conn.release()
+    login.release();
     res.status(200).send({ token })
 })
 
 //liste les produits
 app.get('/produits', async(req, res)=>{
     console.log("test envoie")
-    let conn = await pool.getConnection()
+    const conn = await pool.getConnection()
     let resultat = await conn.query('SELECT * FROM produits')
     res.status(200).json(resultat)
-    conn.release()
+    conn.release();
 
 })
 
